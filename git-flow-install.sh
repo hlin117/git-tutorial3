da568081af0a3b925ba3a081fceb26a90067f2ac
Henry Lin <hlin117@gmail.com>
2015-11-08 10:55:06 -0600

    Added version comments to git flow install

--- git-flow-install.sh
+++ git-flow-install.sh
@@ -1,2 +1,15 @@
 # See http://stackoverflow.com/a/1060886/2014591
+#
+# Tested on:
+#
+# OS X              git version 2.3.4
+# Scientific linux  git version 1.8.4
+#
+# Failed on
+# Mint              git version 1.7.x
+#
+# Acts differently on
+# Ubuntu            git version 1.9.1 (prints to stdout)
+# Mint              git version 1.9.1 (prints to stdout)
+# Scientific linux  git version 1.7.1 (no colors)
 git config --global alias.flow 'log --graph --oneline --all --decorate'
