430617c65bfa1fd8b9c4217ca4edde734417db53
hlin117 <hlin117@gmail.com>
2015-11-09 00:05:51 -0600

    Scaling features of dataset to get better results

--- australian.py
+++ australian.py
@@ -11,13 +11,16 @@ from sklearn.metrics import accuracy_score
 from sklearn.cross_validation import train_test_split
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 
 dataset = pd.read_csv("data/australian.data", header=None, sep=" ")
 X = dataset.drop(14, axis=1)        # Features
 y = dataset[14]                     # Labels
 
 # Produces a "one hot encoding" of the data
-conv_X = pd.get_dummies(X, columns=[0, 3, 4, 5, 7, 8, 10, 11])
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+conv_X = pd.get_dummies(pd.DataFrame(X), columns=[0, 3, 4, 5, 7, 8, 10, 11])
 
 X_train, X_test, y_train, y_test = train_test_split(conv_X, y, test_size=0.25,
                                                     random_state=888)
